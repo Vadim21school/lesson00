d19066de8e1d240fc27530a49cd1e9ec45d365a1
Vadim21school <68219206+Vadim21school@users.noreply.github.com>
2020-11-12 18:53:22 +0300

    Add files via upload

---
+++ ft_memcpy.c
@@ -0,0 +1,28 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void *ft_memcpy(void *destination, const void *source, size_t n)
+{
+	size_t i = 0;
+	while (i < n)
+	{
+		((char *)destination)[i] =  ((char *)source)[i];
+		i++;
+	}
+	return (destination);
+}
+
+void main (void)
+{
+	char src[10] = "123456";
+ 
+   // Массив приемник данных
+	char dst[10] = "";
+
+   // Копируем 6 байт из массива src в массив dst
+   ft_memcpy (dst, src, 6);
+
+   // Вывод массива dst на консоль
+   printf ("dst: %s\n",dst);
+}
